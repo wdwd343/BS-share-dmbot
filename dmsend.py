47982eb9147229aa94af6195b11984a9f33c5704
wdwd343 <45532592+wdwd343@users.noreply.github.com>
2020-06-02 07:12:48 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -12,7 +12,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    game = discord.Game('몽회장 빠빠기')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
@@ -25,13 +25,13 @@ async def on_message(message):
             else:
                 try:
                     msg = message.content[4:]
-                    if message.author.id == ★자신의 디스코드ID를 적어주세요(퍼미션):
-                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="★★최상단 제목★★")
-                        embed.add_field(name="★★제목★★", value=msg, inline=True)
-                        embed.set_footer(text=f"discord.gg/★★서버초대코드★★")
+                    if message.author.id == 551142149777915914:
+                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="카르텔 공지")
+                        embed.add_field(name="필독", value=msg, inline=True)
+                        embed.set_footer(text=f"discord.gg/JZQqs2K")
                         await i.send(embed=embed)
                 except:
                     pass
 
 
-client.run('★★봇토큰★★')
+client.run('NzE3MTM3NDE0OTM3NzcyMTAy.XtV8bQ.PinnuAnct2F9hbnsmv3Ss9tmZTY')
